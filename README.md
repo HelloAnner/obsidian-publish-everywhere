81161b86ba6b0c73f7606df049eb5837777ab27a
anner <anner.wang@icloud.com>
2025-04-10 13:55:45 +0800

    Refactor plugin to publish Obsidian notes to KMS using md2kms tool. Updated settings structure, added publish command, and improved README with installation and usage instructions. Changed plugin name and metadata in manifest and package files.

--- README.md
+++ README.md
@@ -1,94 +1,65 @@
-# Obsidian Sample Plugin
+# Obsidian Publish to KMS
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+这是一个 Obsidian 插件，用于将笔记发布到 KMS 系统。插件使用 md2kms 工具将 Markdown 内容转换并发布到指定的 KMS 页面。
 
-This project uses TypeScript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in TypeScript Definition format, which contains TSDoc comments describing what it does.
+## 功能特点
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+- 支持一键发布当前笔记到 KMS
+- 支持配置 KMS 连接信息（URL、用户名、密码、空间）
+- 支持配置 md2kms 工具路径
+- 支持通过 frontmatter 指定父页面 ID
+- 保持附件相对路径引用
 
-## First time developing plugins?
+## 安装方法
 
-Quick starting guide for new plugin devs:
+1. 下载最新版本的插件文件
+2. 将文件复制到你的 Obsidian 插件目录：`<vault>/.obsidian/plugins/obsidian-publish-kms/`
+3. 在 Obsidian 设置中启用插件
+4. 配置插件设置
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+## 配置说明
 
-## Releasing new releases
+在插件设置中配置以下信息：
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+1. KMS URL：你的 KMS 系统地址
+2. 用户名：KMS 登录用户名
+3. 密码/Token：KMS 登录密码或 API Token
+4. Space：KMS 空间名称
+5. md2kms 路径：md2kms 工具的完整路径
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+## 使用方法
 
-## Adding your plugin to the community plugin list
+1. 在笔记的 frontmatter 中添加 KMS 页面链接，例如：
+   ```yaml
+   ---
+   kms: https://kms.fineres.com/pages/viewpage.action?pageId=1242404331
+   ---
+   ```
 
-- Check the [plugin guidelines](https://docs.obsidian.md/Plugins/Releasing/Plugin+guidelines).
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
+2. 配置快捷键：
+   - 打开设置 > 快捷键
+   - 搜索 "Publish to KMS"
+   - 设置你喜欢的快捷键
 
-## How to use
+3. 发布笔记：
+   - 打开要发布的笔记
+   - 按下配置的快捷键
+   - 等待发布完成
 
-- Clone this repo.
-- Make sure your NodeJS is at least v16 (`node --version`).
-- `npm i` or `yarn` to install dependencies.
-- `npm run dev` to start compilation in watch mode.
+## 注意事项
 
-## Manually installing the plugin
+1. 确保已正确安装并配置 md2kms 工具
+2. 确保笔记中的附件使用相对路径引用
+3. 确保已在 frontmatter 中配置正确的 KMS 页面链接
+4. 发布时会使用当前笔记的文件名作为页面标题
 
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
+## 开发相关
 
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
+- 使用 TypeScript 开发
+- 使用 Obsidian Plugin API
+- 支持实时日志输出
+- 支持错误处理和提示
 
-## Funding URL
+## 许可证
 
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+MIT License
--- main.ts
+++ main.ts
@@ -1,85 +1,55 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { exec } from 'child_process';
+import { App, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, TextComponent } from 'obsidian';
+import * as path from 'path';
+import { promisify } from 'util';
 
-// Remember to rename these classes and interfaces!
+const execAsync = promisify(exec);
 
-interface MyPluginSettings {
-	mySetting: string;
+interface ConfluencePublisherSettings {
+	confluenceUrl: string;
+	username: string;
+	password: string;
+	space: string;
+	md2kmsPath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ConfluencePublisherSettings = {
+	confluenceUrl: '',
+	username: '',
+	password: '',
+	space: '',
+	md2kmsPath: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+interface ProcessResult {
+	stdout: string;
+	stderr: string;
+}
+
+export default class ConfluencePublisher extends Plugin {
+	settings: ConfluencePublisherSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// Add the publish command
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'publish-to-confluence',
+			name: 'Publish current note to Confluence',
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						this.publishCurrentNote(markdownView);
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
+				return false;
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
+		// Add the settings tab
+		this.addSettingTab(new ConfluencePublisherSettingTab(this.app, this));
 	}
 
 	async loadSettings() {
@@ -89,45 +59,174 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async publishCurrentNote(view: MarkdownView) {
+		const file = view.file;
+		if (!file) {
+			console.error('[Publish to Confluence] Error: No file is currently open');
+			new Notice('No file is currently open');
+			return;
+		}
+
+		// Check if settings are configured
+		if (!this.settings.confluenceUrl || !this.settings.username ||
+			!this.settings.password || !this.settings.space || !this.settings.md2kmsPath) {
+			console.error('[Publish to Confluence] Error: Missing configuration', {
+				hasUrl: !!this.settings.confluenceUrl,
+				hasUsername: !!this.settings.username,
+				hasPassword: !!this.settings.password,
+				hasSpace: !!this.settings.space,
+				hasMd2kmsPath: !!this.settings.md2kmsPath
+			});
+			new Notice('Please configure all settings first');
+			return;
+		}
+
+		// Get frontmatter
+		const frontmatter = this.app.metadataCache.getFileCache(file)?.frontmatter;
+
+		if (!frontmatter?.kms) {
+			console.error('[Publish to Confluence] Error: No KMS URL found in frontmatter');
+			new Notice('No KMS URL found in frontmatter');
+			return;
+		}
+
+		// Extract pageId from KMS URL
+		const pageIdMatch = frontmatter.kms.match(/pageId=(\d+)/);
+		if (!pageIdMatch) {
+			console.error('[Publish to Confluence] Error: Could not extract pageId from KMS URL:', frontmatter.kms);
+			new Notice('Could not extract pageId from KMS URL');
+			return;
+		}
+		const parentId = pageIdMatch[1];
+
+		// Get the absolute path of the file
+		const vaultPath = (this.app.vault.adapter as any).basePath;
+		const absoluteFilePath = path.join(vaultPath, file.path);
+
+		try {
+			// Get the title from the file name (without extension)
+			const title = file.basename;
+
+			// Show publishing start message
+			new Notice('⏳ 页面发布中...');
+
+			const command = `${this.settings.md2kmsPath} --url ${this.settings.confluenceUrl} --username ${this.settings.username} --password ${this.settings.password} --space ${this.settings.space} --title ${title} --parent ${parentId} ${absoluteFilePath}`;
+			console.log('[Publish to Confluence] Executing command:', command.replace(this.settings.password, '********'));
+
+			let processOutput = '';
+			let processError = '';
+
+			// Create a promise that resolves when the process ends
+			const processPromise = new Promise<ProcessResult>((resolve, reject) => {
+				const childProcess = exec(command, {
+					maxBuffer: 1024 * 1024 * 10 // 10MB buffer
+				});
+
+				childProcess.stdout?.on('data', (data) => {
+					processOutput += data;
+				});
+
+				childProcess.stderr?.on('data', (data) => {
+					processError += data;
+				});
+
+				childProcess.on('error', (error) => {
+					console.error('[Publish to Confluence] Process error:', error);
+					reject(error);
+				});
+
+				childProcess.on('exit', (code) => {
+					console.log('[Publish to Confluence] Process exited with code:', code);
+					if (code === 0) {
+						resolve({ stdout: processOutput, stderr: processError });
+					} else {
+						reject(new Error(processError || `Process exited with code ${code}`));
+					}
+				});
+			});
+
+			// Wait for the process to complete
+			await processPromise;
+
+			// Show success message
+			new Notice(`✅ 已成功创建页面: ${frontmatter.kms}`);
+
+		} catch (error) {
+			// Show error message with the tool's output
+			new Notice(error.message);
+		}
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ConfluencePublisherSettingTab extends PluginSettingTab {
+	plugin: ConfluencePublisher;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ConfluencePublisher) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'Confluence Publisher Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Confluence URL')
+			.setDesc('Your Confluence instance URL')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+				.setPlaceholder('https://your-domain.atlassian.net')
+				.setValue(this.plugin.settings.confluenceUrl)
+				.onChange(async (value: string) => {
+					this.plugin.settings.confluenceUrl = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Username')
+			.setDesc('Your Confluence username/email')
+			.addText(text => text
+				.setPlaceholder('your.email@domain.com')
+				.setValue(this.plugin.settings.username)
+				.onChange(async (value: string) => {
+					this.plugin.settings.username = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Password/Token')
+			.setDesc('Your Confluence password or API token')
+			.addText((text: TextComponent) => {
+				text.setPlaceholder('Enter your password or API token')
+					.setValue(this.plugin.settings.password)
+					.inputEl.type = 'password';
+				text.onChange(async (value: string) => {
+					this.plugin.settings.password = value;
+					await this.plugin.saveSettings();
+				});
+			});
+
+		new Setting(containerEl)
+			.setName('Space')
+			.setDesc('Your Confluence space key')
+			.addText(text => text
+				.setPlaceholder('SPACEKEY')
+				.setValue(this.plugin.settings.space)
+				.onChange(async (value: string) => {
+					this.plugin.settings.space = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('md2kms Path')
+			.setDesc('Full path to your md2kms executable')
+			.addText(text => text
+				.setPlaceholder('/path/to/md2kms')
+				.setValue(this.plugin.settings.md2kmsPath)
+				.onChange(async (value: string) => {
+					this.plugin.settings.md2kmsPath = value;
 					await this.plugin.saveSettings();
 				}));
 	}
